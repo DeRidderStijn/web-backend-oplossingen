9261de83a27d22d199ebd0a27913c4f2eeafddc5
Stijn <stijn.deridder.1@student.kdg.be>
2015-10-28 10:08:41 +0100

    Deadline1: conditional-statement-if-elseif

---
+++ deadline1/conditional-statement-if-elseif.php
@@ -0,0 +1,62 @@
+<?php
+/*
+Stijn De Ridder
+stijn.deridder.1@student.kdg.be
+*/
+$getal = 40;
+$message;
+
+if ($getal < 10)
+{
+	$message = "Het getal ligt tussen 0 en 10";
+}
+elseif ($getal < 20)
+{
+	$message = "Het getal ligt tussen 10 en 20";
+}
+elseif ($getal < 30)
+{
+	$message = "Het getal ligt tussen 20 en 30";
+}
+elseif ($getal < 40)
+{
+	$message = "Het getal ligt tussen 30 en 40";
+}
+elseif ($getal < 50)
+{
+	$message = "Het getal ligt tussen 40 en 50";
+}
+elseif ($getal < 60)
+{
+	$message = "Het getal ligt tussen 50 en 60";	
+}
+elseif ($getal < 70)
+{
+	$message = "Het getal ligt tussen 60 en 70";
+}
+elseif ($getal < 80)
+{
+	$message = "Het getal ligt tussen 70 en 80";
+}
+elseif ($getal < 90)
+{
+	$message = "Het getal ligt tussen 80 en 90";
+}
+elseif ($getal < 100)
+{
+	$message = "Het getal ligt tussen 90 en 100";
+}
+$message = strrev($message);
+?>
+
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN"
+   "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+   <head>
+      <title>opdracht comments</title>
+   </head>
+   <body>
+      <p> <?php echo($message) ?> </p>
+   </body>
+</html>
