c2c79709bf5720bc1f22e581d7d9f2a101df0bed
Stijn <stijn.deridder.1@student.kdg.be>
2015-10-28 09:17:05 +0100

    Deadline1: opdracht-comments

---
+++ deadline1/comments.php
@@ -0,0 +1,21 @@
+<?php
+/*
+Stijn De Ridder
+stijn.deridder.1@student.kdg.be
+*/
+$voornaam = "Stijn";
+$achternaam = "De Ridder";
+
+
+?>
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN"
+   "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+   <head>
+      <title>opdracht comments</title>
+   </head>
+   <body>
+      <p> voornaam: <?php echo($voornaam) ?> achternaam: <?php echo($achternaam) ?> </p>
+   </body>
+</html>
