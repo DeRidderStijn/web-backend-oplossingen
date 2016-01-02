38976750ac569171cba23c70a26b18880b9f854c
Stijn <stijn.deridder.1@student.kdg.be>
2016-01-02 20:18:15 +0100

    Deadline4: include-require

---
+++ deadline4/include-require/index.php
@@ -0,0 +1,21 @@
+<?php
+	$artikels = array(
+					array(	"title" => "Pijnlijk: loterijspeler loopt miljoenen mis door foutje",
+							"text" => "Hij rekende zich al rijk, de 21-jarige Kamal Maihousane uit het Nederlandse Sittard. De PostcodeKanjer viel gisteren op zijn straat, en Kamal had vier loten. Althans, dat dacht hij. In werkelijkheid had hij helemaal geen loten. Hij had te weinig geld op z'n rekening staan toen de vier loten moesten worden betaald." ,
+							"tag" => "43,9 miljoen"),
+
+					array(	"title" => "Tweeling geboren met verschillende geboortejaren: 2015 én 2016",
+							"text" => " Het komt erg zelden voor: een tweeling met een verschillende geboortedag en zelfs een ander geboortejaar. In San Diego geschiedde het wonder: Jaelin werd geboren in 2015, haar broertje Louis in 2016. Op 31 december 2015 om 23.59 uur kwam Jaelin ter wereld. Louis volgde enkele minuten later en zag het levenslicht op 1 januari 2016 om 0:02 uur.",
+							"tag" => "Twee feestjes"),
+
+					array(	"title" => "Ondanks verbod drinken Iraniërs jaarlijks 60 miljoen liter alcohol",
+							"text" => "Ondanks het alcoholverbod wordt in het streng-islamitische Iran jaarlijks 60 miljoen liter alcohol geconsumeerd. Dat blijkt uit onderzoek van het Iraanse ministerie van Werk en Sociale Zaken. Het ministerie zelf spreekt van een slechte en crisis-achtige evolutie",
+							"tag" =>"drinken")
+					);
+
+	include 'view/header-partial.html';
+
+	include 'view/body-partial.html';
+	
+	include 'view/footer-partial.html';
+?>
