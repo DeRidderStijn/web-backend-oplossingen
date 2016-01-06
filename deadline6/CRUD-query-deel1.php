a247ceeac41e653896e25be584ee80011a94bdf7
Stijn <stijn.deridder.1@student.kdg.be>
2016-01-06 14:49:45 +0100

    Deadline6: CRUD - Query deel 1

---
+++ deadline6/CRUD-query-deel1.php
@@ -0,0 +1,74 @@
+<?php
+   $messageContainer = "";
+
+   try
+   {
+      $db = new PDO('mysql:host=localhost;dbname=bieren', 'root', 'root');
+
+      $querystring = '  SELECT *
+                        FROM bieren 
+                        INNER JOIN brouwers
+                        ON (bieren.brouwernr = brouwers.brouwernr) 
+                        WHERE (bieren.naam LIKE "du%") AND (brouwers.brnaam LIKE "%a%")';
+
+      $statement = $db->prepare($querystring);
+      $statement->execute();
+      $fetchAssoc = array();
+
+      while($row = $statement->fetch(PDO::FETCH_ASSOC))
+      {
+         $fetchAssoc[] = $row;
+      }
+      $columnNames[] = "#";
+      foreach($fetchAssoc[0] as $key => $bier)
+      {
+         $columnNames[] = $key;
+      }
+   }
+   catch(PDOException $e)
+   {
+      $messageContainer = $e->getMessage();
+   }
+?>
+
+<!DOCTYPE html>
+<html>
+   <head>
+      <title>Oplossing oefening 023 - a</title>
+      <style>
+         .even{
+            background-color: green;
+         }
+      </style>
+   </head>
+<body>
+
+      <h1>Voorbeeld van database resultaten ophalen (PDO)</h1> 
+
+      <p><?php echo $messageContainer ?></p>
+
+      <table>
+         <thead>
+            <tr>
+               <?php foreach ($columnNames as $name): ?>
+                  <th><?= $name ?></th>
+               <?php endforeach ?>
+            </tr>
+         </thead>
+
+         <tbody>
+            <?php foreach ($fetchAssoc as $key => $bier): ?>
+            <tr>
+               <td> <?= ($key + 1) ?></td>
+               <?php foreach ($bier as $value): ?>
+                  <td  <?php if(($key + 1) % 2 == 0){echo "class='even'";} ?> ><?= $value ?></td>
+               <?php endforeach ?>
+            </tr>
+            <?php endforeach ?>
+         </tbody>
+
+      </table>
+
+      
+</body>
+</html>
